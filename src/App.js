eb01e1c00f92250c90012792045ecc18997626e7
Erjankydyrov <erjankydyrov29@gmail.com>
2021-04-19 15:42:51 +0600

    added component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import CounterDisplay from './components/CounterDisplay';
 
 function App() {
   return (
     <div className="App">
-      
+      <CounterDisplay />
     </div>
   );
 }
