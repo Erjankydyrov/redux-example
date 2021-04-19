b585db4d989d661f7f136ea67ddf873da88f9dd2
Erjankydyrov <erjankydyrov29@gmail.com>
2021-04-19 15:43:02 +0600

    corected

--- src/components/CounterDisplay.js
+++ src/components/CounterDisplay.js
@@ -1,4 +1,10 @@
+import { useSelector } from "react-redux";
+
 const CounterDisplay = () => {
+    const number = useSelector(state => state.number);
+
+    console.log(number)
+
     return ( 
         <h1>0</h1>
     );
