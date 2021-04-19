255e4bce0e46ac69b129bb85e59488bc45f649c4
Erjankydyrov <erjankydyrov29@gmail.com>
2021-04-19 15:44:47 +0600

    added some news

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -3,6 +3,10 @@ const initialState = {
 }
 
 const counterReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case "increment_by_1":
+            return { ...state, number: state.number + 1 }
+    }
     return state;
 }
 
