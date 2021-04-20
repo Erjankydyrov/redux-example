b86135f0a5a7521c366ab606e8aaceb8b3334542
Erjankydyrov <erjankydyrov29@gmail.com>
2021-04-20 22:37:54 +0600

    added buttons by 10

--- src/components/CounterSimpleControls.js
+++ src/components/CounterSimpleControls.js
@@ -12,10 +12,20 @@ const CounterSimpleControls = () => {
     dispatch({ type: "decrement_by_1" });
   }
 
+  function incrementBy10Callback() {
+    dispatch({ type: "increment_by_10" });
+  }
+  
+  function decrementBy10Callback() {
+    dispatch({ type: "decrement_by_10" });
+  }
+
   return (
     <div>
       <button onClick={incrementBy1Callback}>+1</button>
       <button onClick={decrementBy1Callback}>-1</button>
+      <button onClick={incrementBy10Callback}>+10</button>
+      <button onClick={decrementBy10Callback}>-10</button>
     </div>
   );
 };
