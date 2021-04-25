a8b9a1c0abfa9a81cfbfdc17f2af6d800ad8a169
Erjankydyrov <erjankydyrov29@gmail.com>
2021-04-25 23:25:56 +0600

    added color

--- src/components/TextDisplay.js
+++ src/components/TextDisplay.js
@@ -3,8 +3,10 @@ import { useSelector } from "react-redux";
 const TextDisplay = () => {
     const text = useSelector(store => store.text.text)
     const size = useSelector(store => store.text.size)
+    const color = useSelector(store => store.color.color)
     const style = {
         fontSize: `${size}px`,
+        color: `${color}`
     };
     
     return ( 
